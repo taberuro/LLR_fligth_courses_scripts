a5448fb6c1149ce1042cdcee76dd1ae67162ce8e
taberuro <73286579+taberuro@users.noreply.github.com>
2023-11-07 14:26:00 +0300

    Update qr_read.py

--- fourth_and_sixth_lesson/qr_read.py
+++ fourth_and_sixth_lesson/qr_read.py
@@ -1,3 +1,4 @@
+#lib's 
 import rospy
 from clover import srv
 from std_srvs.srv import Trigger
@@ -7,15 +8,18 @@ from sensor_msgs.msg import Image
 from pyzbar import pyzbar
 import math
 
+#node + bridge
 rospy.init_node('flight')
-
 bridge = CvBridge()
 
+#clover func 
 get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
 navigate = rospy.ServiceProxy('navigate', srv.Navigate)
 land = rospy.ServiceProxy('land', Trigger)
 set_effect = rospy.ServiceProxy('led/set_effect', SetLEDEffect)  # define proxy to ROS-service
 
+
+#def's place
 def navigate_wait(x=0, y=0, z=1, speed=0.5, frame_id='aruco_map', auto_arm=False, tolerance=0.2):
     navigate(x=x, y=y, z=z, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
 
@@ -31,23 +35,25 @@ def navigate_wait(x=0, y=0, z=1, speed=0.5, frame_id='aruco_map', auto_arm=False
         rospy.sleep(0.2)
 
 qr_code = ''
+
 def image_callback(data):
     global qr_code
     cv_image = bridge.imgmsg_to_cv2(data, 'bgr8')
     barcodes = pyzbar.decode(cv_image)
     for barcode in barcodes:
         qr_code = barcode.data.decode("utf-8")
-       
+        
 
 image_sub = rospy.Subscriber("main_camera/image_raw", Image, image_callback, queue_size=1)
 
+
+
+# main func place
 def main():
     print("start")
     navigate_wait(z=1, speed=1, frame_id='body', auto_arm=True)
-    navigate_wait(x=1)
+    navigate_wait(x=3)
     
-   
-
     print("QR = ", qr_code)
     while qr_code == '' and not rospy.is_shutdown():
         rospy.sleep(0.4)
@@ -66,7 +72,7 @@ def main():
 
         print("CHANGING COLOR")
 
-        rospy.sleep(3)
+        rospy.sleep(2)
 
     land()
 
